d7b3f6f38559d1c3da8dea17b5a043b532b0caab
Benjamin Johnson <bjohnson@fcusd.org>
2024-09-04 16:01:23 -0700

    Create notes.py

---
+++ notes.py
@@ -0,0 +1,47 @@
+# More about variables #
+#**Learning objectives**
+#
+#After this section:
+#
+#* You will be able to use variables in different contexts
+#* You will know what kind of data can be stored in variables
+#* You will understand the difference between strings, integers and floating point numbers
+
+
+
+## Live Demo ##
+#
+# Casing
+#name = input("What is your name? ")
+#Name = input("What is your name? ")
+#print(name)
+#print(Name)
+#
+# Talk about Variable Types
+#   * int vs string vs float
+#intNum = 75
+#print(intNum/2)
+#number = "100"
+#print(number / 2)
+#number1 = 2.5
+#number2 = -1.25
+#number3 = 3.62
+#
+#print(f"Mean: {mean}")
+#mean = (number1 + number2 + number3) / 3
+#
+# Printing different types and Casting
+#result = 10 * 25
+#print("The result is " + result)
+#print("The result is " + str(result))
+#print("The result is", result)
+#print(f"The result is {result}") # Format string {} denotes values to be printed
+#name = "Mark"
+#age = 37
+#city = "Palo Alto"
+#print(f"Hi {name}, you are {age} years old. You live in {city}.")
+#
+# Printing invisible things
+#print("\n") # newline
+#print("\t") # tab
+#print("\\") # \
